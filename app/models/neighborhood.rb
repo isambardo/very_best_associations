6ccb50e1476d6f4325028b3bf1008888e64663a9
isambardo <kjanssen@MSPediKjanssen-2.local>
2015-08-16 12:26:30 -0500

    completes part 7

--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -1,5 +1,6 @@
 class Neighborhood < ActiveRecord::Base
   has_many :venues
+  has_many :favorites, :through => :venues
   validates :name, :presence => true
   validates :name, :uniqueness => { :SCOPE => :city }
 end
