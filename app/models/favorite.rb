e70a838910b81d02cc3f66679df4331d90bfe038
isambardo <kjanssen@MSPediKjanssen-2.local>
2015-08-15 21:19:19 -0500

    Completes part 3

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,5 +1,7 @@
 class Favorite < ActiveRecord::Base
-  belongs_to :user, :venue, :dish
+  belongs_to :user
+  belongs_to :venue
+  belongs_to :dish
   validates :user_id, :presence => true
   validates :venue_id, :presence => true
   validates :dish_id, :presence => true
